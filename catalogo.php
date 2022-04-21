26020726770c9f7f1fc18b45a15d4d309c538db5
NOLJ14 <nikolailopezjordan@gmail.com>
2022-04-21 15:21:50 -0400

    Agrego el catalogo y sus imagenes

---
+++ catalogo.php
@@ -0,0 +1,88 @@
+<?php include("includes/header.php")?>
+
+<div id="section1" class="container-fluid bg-light text-dark" style="padding-top:50px;padding-bottom:50px">
+  <div class="row">
+    <div class="col">
+      <br>
+      <center><h2>Consulta</h2></center>
+      <br><br>
+      <p>
+      Dr. Vet cuenta con médicos veterinarios expertos en diferentes disciplinas y con 
+      la metodología necesaria para llegar a los diagnósticos que permitirán enfocar los tratamientos de manera eficiente 
+      y oportuna, seguimos a detalle el expediente clínico orientado a problemas, en el que al conjuntar los datos 
+      generales, la historia clínica y el examen clínico podemos arrojar la lista de problemas que nos permitirá 
+      agrupar los signos de acuerdo a problemas específicos y recomendar las pruebas de laboratorio e imageniología 
+      complementarias.
+      </p>
+           <h5>Precio: 200 Bs.</h5>
+      </div>
+    <div class="col">
+      <br>
+      <center><img src="images/dog1.jpg" alt="Hotel" style="width:300px;height:300px;"></center>
+    </div>
+  </div>  
+</div>
+<div id="section2" class="container-fluid bg-dark text-light" style="padding-top:50px;padding-bottom:50px">
+  <div class="row">
+    <div class="col">
+      <center><h2>Medicina Preventiva</h2></center>
+      <p>
+      En Dr. Vet contamos con excelentes programas de medicina preventiva como es 
+      la aplicación de vacunas para perros, gatos y hurones de los laboratorios más reconocidos y con los distribuidores  
+      del ramo que se han destacado por el mantenimiento de la cadena fría para que los biológicos al ser aplicados a su 
+      mascota se encuentren en perfectas condiciones.
+      También contamos con el chequeo geriátrico el cual recomendamos para perros de talla miniatura a 
+      partir de los 9 años de edad, los perros de talla media a partir de los 7 años y los de talla gigante como el 
+      gran danés o san bernardo a partir de los 6 años, en estos chequeos podemos detectar enfermedades antes de la 
+      presentación de los signos clínicos y comenzar una estrategia para que nuestros amigos tengan una vida más 
+      larga y de mejor calidad.
+        </p>     
+          <h5>Precio: 340 Bs.</h5>
+    </div>
+    <div class="col">
+      <br>
+      <center><img src="images/dog2.jpg" alt="Hotel" style="width:500px;height:400px;"></center>  
+    </div>
+  </div>
+  
+</div>
+<div id="section41" class="container-fluid bg-light text-dark" style="padding-top:20px;padding-bottom:20px">
+<br> 
+<h2>Peluqueria Canina</h2>
+  <div class="row">
+    <div class="col">
+      <br>
+      <p>
+      Dr. Vet cuenta con expertos estilistas en el arreglo de todas las 
+      razas de perros y gatos, las mascotas son bañadas con agua caliente y los shampoo y jabones de la más alta calidad.
+      </p>
+      <h5>Precio: 450 Bs.</h5>
+    </div>
+    <div class="col">
+      <center><img src="images/dog3.jpg" alt="Hotel" style="width:300px;height:200px;"></center>  
+    </div>
+  </div>
+</div>
+<div id="section42" class="container-fluid bg-dark text-light" style="padding-top:50px;padding-bottom:50px">
+  <h2>Imagenología</h2>
+  <div class="row">
+    <div class="col">
+        <br>
+        <p>
+        Dr. Vet cuenta con métodos diagnósticos de alta calidad como son radiología de 
+        alta frecuencia que es un equipo de rayos X gentil con el medio ambiente y con mucho menor riesgo para el 
+        personal que labora en el área, cumpliendo con las normas oficiales mexicanas al respecto. Se cuenta también 
+        con ecosonografía y endoscopía de fibra óptica, estas técnicas complementarias ayudan a tener métodos 
+        diagnósticos confiables para enfocar la terapia de manera eficiente. Contamos con los equipos más modernos 
+        en imagen tal es el caso de radiología digital el cual permite alta definición en imagen radiología y 
+        ultrasonografía con doppler y 3ra. dimensión.
+        </p>
+        <h5>Precio: 650 Bs.</h5>
+      </div>
+    <div class="col">
+      <center><img src="images/dog4.jpg" alt="Hotel" style="width:400px;height:400px;"></center>  
+    </div>
+  </div>  
+</div>
+</html>
+<?php include("includes/footer.php")?>
